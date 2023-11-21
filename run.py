0e01027fd5ea73913c2add856180de502e768a5f
Aaron080913 <>
2023-11-21 22:20:40 +0000

    add validation checks for 6 values in input data

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Carvery_List')
 
+
 def get_sales_data():
     """
     Get sales input from the user
@@ -19,9 +20,24 @@ def get_sales_data():
     print("Please enter carveries ordered from the last Sunday.")
     print("Data should be six numbers, seperated by commas.")
     print("Example: 5,10,15,20,25,30\n")
-
     order_str = input("Enter your orders here:")
-    print(f"The orders are as follows {order_str}")
+    sales_data = order_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    converts all strings into integers.
+    shows valueerror if string cannot be
+    converted into an integer, or if not 6 number.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly six values required, you provided {len(values)}"
+            )  
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 
 get_sales_data()
